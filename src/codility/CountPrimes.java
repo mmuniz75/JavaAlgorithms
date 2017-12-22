f3b7fffbfb31b169f48af5974adb7edaea117ff7
mmuniz75 <muniz_marcelo@hotmail.com>
2017-12-22 18:32:36 -0200

    prime number count

---
+++ src/codility/CountPrimes.java
@@ -0,0 +1,39 @@
+package codility;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class CountPrimes {
+	
+	@Test
+	public void test(){
+		assertEquals(4,solution(10));
+	}
+	
+	@Test
+	public void test2(){
+		assertEquals(7,solution(17));
+	}
+	
+		
+	private int solution(int n){
+		int[] sieve = new int[n+1];
+		Arrays.fill(sieve,1);
+		sieve[0] = 0;
+		sieve[1] = 0;
+		
+		for(int i=2;i <= Math.sqrt(n);i++){
+			if (sieve[i]==1){
+				for (int k=i*i;k <= n;k+=i) 
+					sieve[k] = 0;
+					
+			}	
+		}
+		
+		return (int)Arrays.stream(sieve).filter(s -> s==1).count();
+	}
+
+}
